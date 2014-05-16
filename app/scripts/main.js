8ab14eb98bf3a31583dfae5d2bff715ecedea542
yutaono <vrf.ced@gmail.com>
2014-05-16 15:54:37 +0900

    added key command.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,34 @@
-$(function(){
-  var speed = 10;
+'use strict';
 
-  var s = 0;
-  setInterval(function(){
-    s += speed;
-    $("html, body").animate({scrollTop: s}, 100);
-  }, 100);
+$(function(){
+    var isScrolling = false;
+    var timer;
+    var s = 0;
+
+    function startTimer() {
+        var speed = 10;
+
+        timer = setInterval(function(){
+            s += speed;
+            $('html, body').animate({scrollTop: s}, 100);
+        }, 100);
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    $('body').on('keyup', function(e){
+        if (e.keyCode !== 76) {  // 76はLの文字コード
+            return;
+        }
+
+        if(isScrolling) {
+            isScrolling = false;
+            stopTimer();
+        } else {
+            isScrolling = true;
+            startTimer();
+        }
+    });
 });
